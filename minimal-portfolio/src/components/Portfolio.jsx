836668430fd55698cc09a34db43ff2bed0130ec1
William Luo <william2299@gmail.com>
2024-01-30 08:34:43 -0700

    ok

--- minimal-portfolio/src/components/Portfolio.jsx
+++ minimal-portfolio/src/components/Portfolio.jsx
@@ -6,7 +6,7 @@ function Portfolio() {
   return (
     <div className='flex flex-col md:flex-row items-center justify-center'>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '>
-
+      {portfolio.map(project )}
       </div>
     </div>
   )

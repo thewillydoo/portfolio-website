1b43e645e242b7c092664580b80308f3a547d280
William Luo <william2299@gmail.com>
2024-01-31 06:41:38 -0700

    created timeline

--- minimal-portfolio/src/components/Timeline.jsx
+++ minimal-portfolio/src/components/Timeline.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import TimelineItem from './TimelineItem'
+import timeline from '../data/timeline.js'
+
 
 function Timeline() {
   return (
-    <div>
-      
+    <div className='flex flex-col md:flex-row justify-center my-20 w-full md:w-7/12'>
+      {timeline.map(item => (
+        <TimelineItem 
+          year={item.year}
+          title={item.title}
+          duration={item.duration}
+          details={item.details}
+        />
+      ))}
     </div>
   )
 }
---
+++ minimal-portfolio/src/data/timeline.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    year: "Today",
+    title: "creator of this website",
+    duration: "5 years",
+    details:
+      "I created this website using React, Tailwind, and Vite. I also used the Vite plugin react-svg to import SVGs as React components.",
+  },
+  {
+    year: "Today",
+    title: "creator of this website",
+    duration: "5 years",
+    details:
+      "I created this website using React, Tailwind, and Vite. I also used the Vite plugin react-svg to import SVGs as React components.",
+  },
+  {
+    year: "Today",
+    title: "creator of this website",
+    duration: "5 years",
+    details:
+      "I created this website using React, Tailwind, and Vite. I also used the Vite plugin react-svg to import SVGs as React components.",
+  },
+  {
+    year: "Today",
+    title: "creator of this website",
+    duration: "5 years",
+    details:
+      "I created this website using React, Tailwind, and Vite. I also used the Vite plugin react-svg to import SVGs as React components.",
+  },
+];

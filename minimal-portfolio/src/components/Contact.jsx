24a2e3c2cd9d80c80f7b8cf1fc199ed8670e99bd
William Luo <william2299@gmail.com>
2024-01-31 08:39:41 -0700

    finished timeline and working on contact

--- minimal-portfolio/src/components/Contact.jsx
+++ minimal-portfolio/src/components/Contact.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>
+    <div className='flex flex-col mb-10 mx-auto'>
+      <div className='flex justify-center items-center'>
+        <form 
+          className='flex flex-col w-full md:w-1/2'
+          action='https://getform.io/slug'
+          method='POST'
+        >
+          <input 
+            type='text'
+            name='name'
+            placeholder='Name'
+            className='p-2 bg-transparent border-2 rounded-md focus:outline-none'
+          />
+          <input 
+            type='text'
+            name='email'
+            placeholder='Email'
+            className='my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none'
+          />
+          <textarea 
+          name='message'
+          placeholder='Message'
+          rows='10'
+          className=''
+          >
+
+          </textarea>
+        </form>
+
+      </div>
       
     </div>
   )
--- minimal-portfolio/src/components/Timeline.jsx
+++ minimal-portfolio/src/components/Timeline.jsx
@@ -5,15 +5,18 @@ import timeline from '../data/timeline.js'
 
 function Timeline() {
   return (
-    <div className='flex flex-col md:flex-row justify-center my-20 w-full md:w-7/12'>
-      {timeline.map(item => (
-        <TimelineItem 
-          year={item.year}
-          title={item.title}
-          duration={item.duration}
-          details={item.details}
-        />
-      ))}
+    <div className='flex flex-col md:flex-row justify-center my-20'>
+      <div className='w-full md:w-7/12'>
+        
+        {timeline.map(item => (
+          <TimelineItem 
+            year={item.year}
+            title={item.title}
+            duration={item.duration}
+            details={item.details}
+          />
+        ))}
+      </div>
     </div>
   )
 }

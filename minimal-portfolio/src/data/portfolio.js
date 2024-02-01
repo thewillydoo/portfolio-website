37397d5d9cd6c30e415d99a31b4e76fd367a071e
William Luo <william2299@gmail.com>
2024-02-01 05:10:38 -0700

    added some items and images

--- minimal-portfolio/src/data/portfolio.js
+++ minimal-portfolio/src/data/portfolio.js
@@ -3,19 +3,19 @@
     {
         title: 'The Conversationalist',
         imgUrl: '/assets/images/theconversationalist.PNG',
-        stack: ['TypeScript', 'Next.js', 'Docker'],
+        stack: ['React', 'Next.js', 'TypeScript','Google Cloud','OpenAI API', 'Docker'],
         link: 'https://reformed-22an6t4ebq-uc.a.run.app/'
     },
     {
         title: 'Portfolio Website',
-        imgUrl: '',
-        stack: ['React', 'Tailwind', 'Vite'],
+        imgUrl: '/assets/images/portfoliowebsite.PNG',
+        stack: ['React','JavaScript', 'Tailwind CSS', 'Vite'],
         link: ''
     },
     {
         title: 'Cancer Prediction using Machine Learning',
         imgUrl: '/assets/images/cancerprediction.PNG',
-        stack: ['Python', 'TypeScript', 'Next.js'],
+        stack: ['Python', 'Pandas', 'NumPy', 'Machine Learning'],
         link: ''
     }
  ]

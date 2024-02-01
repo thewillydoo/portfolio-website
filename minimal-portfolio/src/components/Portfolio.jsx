4d6b50262e70874bebecaf2cb0a88e58e86b69f1
William Luo <william2299@gmail.com>
2024-02-01 03:33:37 -0700

    added dark mode

--- minimal-portfolio/src/components/Portfolio.jsx
+++ minimal-portfolio/src/components/Portfolio.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import PortfolioItem from './PortfolioItem'
-import portfolio from '../data/portfolio.js'
+import React from 'react';
+import portfolio from '../data/portfolio';
+import PortfolioItem from './PortfolioItem';
 
 function Portfolio() {
-  return (
-    <div className='flex flex-col md:flex-row items-center justify-center'>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-      {portfolio.map(project => (
-        <PortfolioItem 
-        
-          imgUrl={project.imgUrl}
-          title={project.title}
-          stack={project.stack}
-          link={project.link} 
-          />
-      ))}
+   return (
+      <div className="flex flex-col md:flex-row items-center justify-center">
+         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {portfolio.map(project => (
+               <PortfolioItem 
+                  imgUrl={project.imgUrl}
+                  title={project.title}
+                  stack={project.stack}
+                  link={project.link}
+               />
+            ))}
+         </div>
       </div>
-    </div>
-  )
+   )
 }
 
-export default Portfolio
+export default Portfolio;

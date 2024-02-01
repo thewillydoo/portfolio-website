75c29d7c719ea032c5147064df6753cffee88baf
William Luo <william2299@gmail.com>
2024-02-01 13:13:01 -0700

    timeline changes

--- minimal-portfolio/src/data/timeline.js
+++ minimal-portfolio/src/data/timeline.js
@@ -1,30 +1,30 @@
 export default [
   {
-    year: "Today",
-    title: "creator of this website",
-    duration: "5 years",
+    year: "2024",
+    title: "Undergraduate Artificial Intelligence Society Member",
+    duration: "1 year",
     details:
-      "I created this website using React, Tailwind, and Vite. I also used the Vite plugin react-svg to import SVGs as React components.",
+      "The Undergraduate Artificial Intelligence Society at the University of Alberta is a group of students interested about exploring, learning about, and applying various Artificial Intelligence techniques of all kinds. As an active member, I have successfully completed a open-source project within this dynamic environment, fostering substantial learning experiences and skill development.",
   },
   {
-    year: "Today",
-    title: "creator of this website",
-    duration: "5 years",
+    year: "2023",
+    title: "Software Engineer Intern",
+    duration: "1 year",
     details:
-      "I created this website using React, Tailwind, and Vite. I also used the Vite plugin react-svg to import SVGs as React components.",
+      "I developed a functional and user friendly 3D printing database web application for Alberta Health Service professionals. I used React, JavaScript, Node.js, and Django. I also used Bootstrap for styling and deployed the application on AWS.",
   },
   {
-    year: "Today",
-    title: "creator of this website",
-    duration: "5 years",
+    year: "2021",
+    title: "Computer Engineering @ University of Alberta",
+    duration: "3 years",
     details:
-      "I created this website using React, Tailwind, and Vite. I also used the Vite plugin react-svg to import SVGs as React components.",
+      "I was accepted into the Electrical and Computer Engineering program at the University of Alberta. Now, I am currently in my third year. I have learned a lot about electronics, computer architecture, and programming.",
   },
   {
-    year: "Today",
-    title: "creator of this website",
-    duration: "5 years",
+    year: "2016",
+    title: "First Line of Code",
+    duration: "The beginning",
     details:
-      "I created this website using React, Tailwind, and Vite. I also used the Vite plugin react-svg to import SVGs as React components.",
+      "I wrote my first line of code in JavaScript. I was 12 years old and I was trying to automate a boring task. I was hooked from that moment on.",
   },
 ];

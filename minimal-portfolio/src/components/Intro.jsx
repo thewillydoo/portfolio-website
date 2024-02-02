e9850b6356bd143aa2c71f80c6d0c9b424039b51
William Luo <william2299@gmail.com>
2024-02-01 23:37:17 -0700

    finalized intro

--- minimal-portfolio/src/components/Intro.jsx
+++ minimal-portfolio/src/components/Intro.jsx
@@ -6,12 +6,11 @@ function Intro() {
       <h1 className='text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold'>William Luo</h1>
       <p className='text-base md:text-xl mb-3 font-medium'>Software Engineering Student</p>
       <p className='text-sm max-w-xl mb-6 font-bold'>
-      I am a Software Engineering student studying at the University of Alberta
-      with a passion for full-stack development and machine learning. 
-      I have a strong understanding of both front-end and back-end development, 
-      and I am excited to put my skills to use in a professional setting. 
-      A quick learner and team player, I am confident that I can be a valuable 
-      asset to any team. 
+      I am a Software Engineering student studying at the University of Alberta with a passion for full-stack development and deep learning. 
+      I have a strong understanding of both front-end and back-end technologies, and I am super excited to put my skills to use in a professional setting. 
+      I am constantly improving my skills by working on new projects to enhance my abilities in my free time. Some things I enjoy 
+      indulging in my free time are working out, playing video games with friends or watching the latest shows/movies.
+      Currently, I am seeking a summer internship for 2024. Can't wait to connect with you!
        </p>
     </div>
   )

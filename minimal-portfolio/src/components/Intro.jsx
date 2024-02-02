02c11220ba56867755b48de70fa696ba3d2ba2e7
William Luo <william2299@gmail.com>
2024-02-02 03:09:22 -0700

    ok

--- minimal-portfolio/src/components/Intro.jsx
+++ minimal-portfolio/src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Intro() {
   return (
     <div className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
-      <h1 className='text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold'>William Luo</h1>
+      <h1 className='text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold'>William Lu</h1>
       <p className='text-base md:text-xl mb-3 font-medium'>Software Engineering Student</p>
       <p className='text-sm max-w-xl mb-6 font-bold'>
       I am a Software Engineering student studying at the University of Alberta with a passion for full-stack development and deep learning. 
